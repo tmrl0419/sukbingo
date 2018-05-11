e1416720c88a2bdd2e5ce289d4694ad948db97ba
tmrl0419 <them0419@naver.com>
2018-05-12 04:58:11 +0900

    Update crawling.py
    add image crawling

--- crawling.py
+++ crawling.py
@@ -12,15 +12,6 @@ def spider_ingredients(url):
     list = (str(parse)).split(" ")
     return list
 
-def spider_description(url):
-    source_code = requests.get(url)
-    plain_text = source_code.text
-    soup = BeautifulSoup(plain_text,'html.parser')
-    string2 = soup.findAll('p', "txt")
-    #parse = re.sub('(|-|=|.|#|/|?|:|$|}|)|[|]|||를', '', str(string))
-    temp = re.sub('<p.*?>|</p>', '', str(string2[0]), 0, re.I|re.S)
-    print(temp)
-
 def spider_name(url):
     source_code = requests.get(url)
     plain_text = source_code.text
@@ -30,11 +21,23 @@ def spider_name(url):
     temp = temp.split('"')
     temp = str(temp[1])
     temp = temp.split()
+    return str(temp[0])
 
+def spider_img(url):
+    source_code = requests.get(url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text,'html.parser')
+    string = soup.find(property = "og:image" )
+    temp = str(string)
+    temp = temp.split('"')
+    temp = str(temp[1])
+    temp = temp.split()
     return str(temp[0])
 
 if __name__ == '__main__':
-    print("test!!")
-    list = spider_ingredients('http://terms.naver.com/entry.nhn?docId=1989304&cid=42785&categoryId=44169')
-    print(list)
-    spider_name('http://terms.naver.com/entry.nhn?docId=1989868&cid=42785&categoryId=44169')
+    #print("test!!")
+    #list = spider_ingredients('http://terms.naver.com/entry.nhn?docId=1989304&cid=42785&categoryId=44169')
+    #print(list)
+    #spider_name('http://terms.naver.com/entry.nhn?docId=1989868&cid=42785&categoryId=44169')
+    str = spider_img('https://terms.naver.com/entry.nhn?docId=1989309&cid=48163&categoryId=48201')
+    print(str)
